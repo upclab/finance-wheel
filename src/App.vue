3be55921d9ea665358e483ed1d43d972fdb4f270
Diego Jara <saintplay96@gmail.com>
2017-09-27 09:04:28 -0500

    Hello World

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="scss">
+@import "node_modules/bulma/sass/utilities/initial-variables";
+
+$blue: #76AED2;
+$red: #FD9898;
+$purple: #8b76d2;
+
+$family-sans-serif: BlinkMacSystemFont, -apple-system, "Lato", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+
+$primary: $purple;
+$danger: $red;
+
+@import "node_modules/bulma/bulma";
+@import "assets/sass/utils/all";
+
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="columns is-vcentered">
+    <div class="column is-8 has-text-centered">
+      <img src="./../assets/logo.png">
+    </div>
+    <div class="column is-4">
+      <div class="hero is-fullheight bg-primary">
+        <div class="hero-body">
+          <div class="container">
+            <div class="columns">
+              <div id="right-column" class="column is-10 is-offset-1 has-text-right">
+                <div>
+                  <h1 class="title has-text-white">Bienvenid@!</h1>
+                  <h2 class="subtitle has-text-white">a tu asistente de finanzas!</h2>
+                </div>
+                <div class="content is-medium is-self-aligned-end">
+                  <ul class="has-no-decoration">
+                    <li><a class="has-text-white" href="http://github.com/upclab/finance-wheel" target="_blank">Acerca De</a></li>
+                    <li><a class="has-text-white" href="http://github.com/upclab/finance-wheel" target="_blank">Repositorio</a></li>
+                    <li><a class="has-text-white" href="http://github.com/upclab/finance-wheel/issues" target="_blank">Dudas</a></li>
+                  </ul>
+                </div>
+                <div class="is-self-aligned-end">
+                  <button class="button is-primary is-inverted is-outlined is-large">Empezar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'welcome',
+};
+</script>
+
+<style lang="scss" scoped>
+  #right-column {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    min-height: 523px;
+
+      div {
+        width: 100%;
+      }
+  }
+
+  ul {
+    position: relative;
+  }
+
+
+  li {
+    transition: all 0.4s ease;
+
+    &:hover {
+      transform: translateX(-25px);
+    }
+  }
+
+</style>
