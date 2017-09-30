001550e185f004a6b83ea7cece7e89398999427d
Diego Jara <saintplay96@gmail.com>
2017-09-29 21:27:26 -0500

    Add electron build

--- config/index.js
+++ config/index.js
@@ -34,5 +34,8 @@ module.exports = {
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
     cssSourceMap: false
+  },
+  app: {
+    index: path.resolve(__dirname, '../package/index.html'),
   }
 }
