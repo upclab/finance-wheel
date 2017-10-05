722711ca3a3a437d66ea78e635fb8db567d9193c
Diego Jara <saintplay96@gmail.com>
2017-10-04 21:57:41 -0500

    web prod build restored

--- build/build.web.js
+++ build/build.web.js
@@ -2,41 +2,33 @@
 
 require('./check-versions')();
 
-const ora = require('ora');
 const rm = require('rimraf');
 const path = require('path');
 const chalk = require('chalk');
 const webpack = require('webpack');
+
 const config = require('../config');
+const greeting = require('./Utils/greeting');
+const logStats = require('./Utils/logStats');
 const webpackConfig = require('./webpack.web.prod.conf');
 
 process.env.NODE_ENV = 'production';
 
-const spinner = ora('building for production...');
-spinner.start();
+greeting();
+logStats('Web Production', chalk.white.bold('Starting builidng for production'));
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), (error) => {
   if (error) throw error;
   webpack(webpackConfig, (err, stats) => {
-    spinner.stop();
     if (err) throw err;
-    process.stdout.write(`${stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false,
-    })}\n\n`);
+
+    logStats('Web Production', stats);
 
     if (stats.hasErrors()) {
       console.log(chalk.red('  Build failed with errors.\n'));
       process.exit(1);
     }
 
-    console.log(chalk.cyan('  Build complete.\n'));
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n',
-    ));
+    logStats('Web Production', chalk.green('Build complete'));
   });
 });
